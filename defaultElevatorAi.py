9070879c9ad08ac06ebaff5ef2b4e9afb825fe55
Max DeLiso <maxdeliso@gmail.com>
2012-12-18 01:15:53 -0500

    added debugging in default script

--- defaultElevatorAi.py
+++ defaultElevatorAi.py
@@ -10,5 +10,11 @@ def computeNextMove(floors, elevators):
    # the C++ code on the other end of this function expects a tuple of tuples
    # of size 2, not a tuple of ints.
    
+   # write state of input to the file for verification
+   dataOut = open("pythonScriptData.txt", "a")
+   dataOut.write( "floors: " + str(floors) + " elevators: " + str(elevators) + "\n" )
+   dataOut.close()
+   
+   # return a random dispatch (for now)
    return ( ( random.randint(0, numElevs-1),
               random.randint(0, numFloors-1) ), ) 
